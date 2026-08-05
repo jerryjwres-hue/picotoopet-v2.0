309903f1207d12cbe635f055a96b7697de122dd3
jerryjwres-hue <jerryjwres@gmail.com>
2026-08-05 12:36:17 -0400

    feat: add cloud development page code-behind

---
+++ windows/desktop/src/PicotooPet.Desktop/Views/Pages/CloudDevelopmentPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PicotooPet.Desktop.Views.Pages;
+
+/// <summary>冻结 Handoff / Return Contract 的只读原生页面。</summary>
+public partial class CloudDevelopmentPage : UserControl
+{
+    public CloudDevelopmentPage()
+    {
+        InitializeComponent();
+    }
+}
