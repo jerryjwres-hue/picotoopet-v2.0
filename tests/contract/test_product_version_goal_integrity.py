8422be551001969223d3eaddbd8e96b2fd4c378d
jerryjwres-hue <jerryjwres@gmail.com>
2026-08-11 23:47:08 -0400

    test: advance canonical version integrity to 2.3.22.1

--- tests/contract/test_product_version_goal_integrity.py
+++ tests/contract/test_product_version_goal_integrity.py
@@ -16,7 +16,7 @@ def test_goal_contract_names_canonical_product_version_source_and_payload() -> N
     version = contract["windows"]["product_version"]
 
     assert version == {
-        "value": "2.3.21.1",
+        "value": "2.3.22.1",
         "source_path": "src/picotoopet_core/product-version.txt",
         "payload_path": "product-version.txt",
     }
