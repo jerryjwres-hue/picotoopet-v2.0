9ab8c396b995492b0703409d8e136f0d265730a3
jerryjwres-hue <jerryjwres@gmail.com>
2026-08-13 22:28:56 -0400

    feat: add operator review view

---
+++ windows/desktop/src/PicotooPet.Desktop/Views/Pages/OperatorReviewPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PicotooPet.Desktop.Views.Pages;
+
+public partial class OperatorReviewPage : UserControl
+{
+    public OperatorReviewPage()
+    {
+        InitializeComponent();
+    }
+}
