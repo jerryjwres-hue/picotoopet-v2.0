d9996bebc9dfbb4b4e0d05993ba3519a354a3deb
jerryjwres-hue <jerryjwres@gmail.com>
2026-08-15 17:24:39 -0400

    feat: add standalone Research Gateway package

---
+++ research_gateway/__init__.py
@@ -0,0 +1,5 @@
+"""PicotooPet standalone Research Gateway."""
+
+from .gateway import READ_CAPABILITIES, CommandResult, GatewayDispatcher, PolicyError
+
+__all__ = ["READ_CAPABILITIES", "CommandResult", "GatewayDispatcher", "PolicyError"]
