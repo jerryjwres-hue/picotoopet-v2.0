b82d54908e9a5ddba49cd3c3b7440932655da26f
jerryjwres-hue <jerryjwres@gmail.com>
2026-08-15 18:39:59 -0400

    test: register Maotai yawn continuity gate

--- windows/desktop/tests/PicotooPet.Desktop.Core.SmokeTests/Program.cs
+++ windows/desktop/tests/PicotooPet.Desktop.Core.SmokeTests/Program.cs
@@ -35,6 +35,7 @@ internal static class Program
             MaotaiNaturalMotionV2SmokeTests.Run();
             MaotaiRenderLoopPerformanceSmokeTests.Run();
             MaotaiJumpDynamicsSmokeTests.Run();
+            MaotaiYawnTransitionSmokeTests.Run();
             MaotaiNaturalMotionV2AcceptanceSmokeTests.Run();
             MaotaiAssetPixelValidationSmokeTests.Run();
             MaotaiNaturalExpressionV2SmokeTests.Run();
